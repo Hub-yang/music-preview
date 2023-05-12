7cbcbbb6950a9432909feedc3adee1adcd06ab96
mocheng <18830279823@163.com>
2023-05-12 10:32:29 +0800

    remove: server

--- Music Preview node_server/src/router.js
+++
@@ -1,9 +0,0 @@
-let express = require("express")
-let router = express.Router()
-let baseRequest = require("./api")
-
-router.get("/getFileList", baseRequest.getFileList)
-router.get("/getPreUrl", baseRequest.getPreUrl)
-router.get("/download", baseRequest.download)
-
-module.exports = router
